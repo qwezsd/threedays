43bfbcc6c2bf3685861144a8b1bd4e1e3d536e74
qwezsd <avwds@naver.com>
2024-08-08 21:01:59 +0900

    threedays

--- src/WrapComponent.jsx
+++ src/WrapComponent.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './wrap.scss'
 import HeaderComponent from './wrap/HeaderComponent';
 import ModalComponent from './wrap/ModalComponent';
 
 export default function WrapComponent () {
+
+    const [state, setState] = useState({
+        modal : false
+    })
+
+    const ModalClick = () => {
+        setState({
+            ...state,
+            modal:true
+        })
+    }
+
     return (
         <div id='wrap'>
             <HeaderComponent />
-            <ModalComponent />
+            {state.modal &&
+                <ModalComponent />
+            }
         </div>
     );
 };
