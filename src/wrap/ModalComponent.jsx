a2cc2040d2415b96ebbc7e0b4cd68f61637d1495
qwezsd <avwds@naver.com>
2024-08-08 20:16:01 +0900

    threedays

--- src/wrap/ModalComponent.jsx
+++ src/wrap/ModalComponent.jsx
@@ -9,7 +9,18 @@ export default function ModalComponent ()  {
                     <input type="text" placeholder='목표 설정하삼' />
                 </div>
                 <div className="content">
-                    <input type="text" placeholder='며칠 실행?' />
+                    <div className="gap">
+                        <label htmlFor="3일">3일</label>
+                        <input type="radio" id='3일' />
+                    </div>
+                    <div className="gap">
+                        <label htmlFor="6일">6일</label>
+                        <input type="radio" id='6일' />
+                    </div>
+                    <div className="gap">
+                        <label htmlFor="9일">9일</label>
+                        <input type="radio" id='9일' />
+                    </div>
                 </div>
             </div>
         </div>
