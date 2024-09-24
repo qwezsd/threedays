64c9a91f1ce83a9f0439fd7bef55aa73b2829127
qwezsd <avwds@naver.com>
2024-09-24 20:59:52 +0900

    threedays

---
+++ src/wrap/noGoalComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function noGoalComponent () {
+    return (
+        <div id='noGoal'>
+            <div className="container">
+                <div className="content">
+                    <title>
+                        <h2>목표 설정을 해주세요 다음에 표정을 선택해주세요 </h2>
+                        둘 다 안 했으면 목표 설정 먼저 하고 그거 채워지면 표정 선택하기
+                    </title>
+                </div>
+            </div>
+        </div>
+    );
+};
+
