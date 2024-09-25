8e6e4181b40148a613bfff3d355cfb7cdf41883a
qwezsd <avwds@naver.com>
2024-09-25 22:50:10 +0900

    threedays

--- src/wrap/noGoalComponent.jsx
+++ src/wrap/noGoalComponent.jsx
@@ -7,7 +7,7 @@ export default function noGoalComponent () {
                 <div className="content">
                     <title>
                         <h2>목표 설정을 해주세요 다음에 표정을 선택해주세요 </h2>
-                        둘 다 안 했으면 목표 설정 먼저 하고 그거 채워지면 표정 선택하기
+                        {/* 둘 다 안 했으면 목표 설정 먼저 하고 그거 채워지면 표정 선택하기 */}
                     </title>
                 </div>
             </div>
